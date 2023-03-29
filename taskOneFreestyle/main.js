4426d4c27cc0b0910077da4b7dccf0e6a112f88f
eyadal <31968847+eyadal@users.noreply.github.com>
2023-03-29 18:36:01 +0200

    add 3 buttons, create three category arrays, create a page info array, create eventhandlers for each button, create a div with an id in the index.html, append html elements to pageInfoElement

--- taskOneFreestyle/main.js
+++ taskOneFreestyle/main.js
@@ -1,5 +1,6 @@
 let listOfIngredientsElement = $('.list-of-ingredients')
-let ingredients = [
+let pageInfoElement = $('#page-info')
+let dairy = [
   {
     product: 'Milk',
     volume: '1 Liter',
@@ -7,36 +8,176 @@ let ingredients = [
     image: 'https://www.torebrings.se/bilder/artikelbilder/10/104989mega.jpg',
   },
   {
-    product: 'Cheese',
-    volume: '1 Kg',
-    price: 155,
-    image:
-      'https://res.cloudinary.com/coopsverige/images/e_sharpen,f_auto,fl_clip,fl_progressive,q_90,c_lpad,g_center,h_330,w_330/v1646291894/cloud/248366/Ost%20Humlan%20Familjefavoriter%20Eko%20.jpg',
+    product: 'Fil',
+    volume: '1 Liter',
+    price: 17,
+    image: 'https://www.kaka.se/product/image/medium/27087_0.jpg',
   },
   {
-    product: 'Chicken',
-    volume: '1 Kg',
-    price: 180,
-    image:
-      'https://aldprdproductimages.azureedge.net/media/resized/$Aldi_GB/ALL_RESIZED3/4088600348513_0_XL.png',
+    product: 'Yoghurt',
+    volume: '1 Liter',
+    price: 14,
+    image: 'https://sportscater.se/wp-content/uploads/2015/10/361932.jpg',
   },
   {
-    product: 'Apples',
+    product: 'Kvarg',
     volume: '1 kg',
     price: 23,
     image:
-      'https://appelkaka.nu/sites/appelkaka.nu/pages/appelsorter/granny_smith/appelsorter_granny_smith.jpg',
+      'https://d1ax460061ulao.cloudfront.net/1000x1000/a/6/a6753583961f9ba13c2b1d5a7290723e.jpg',
   },
   {
-    product: 'Strawberries',
-    volume: '1 Liter',
-    price: 60,
+    product: 'Keso',
+    volume: '500g',
+    price: 20,
     image:
-      'https://www.unh.edu/unhtoday/sites/default/files/styles/article_huge/public/article/2017/unhorganicstrawharvest.jpg?itok=8lk_pisW',
+      'https://res.cloudinary.com/coopsverige/image/upload/v1652703161/cloud/252149.jpg',
+  },
+]
+let fruits = [
+  {
+    product: 'Banana',
+    volume: '1 kg',
+    price: 22,
+    image:
+      'https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/51ebZJ+DR4L._SL1500_.jpg',
+  },
+  {
+    product: 'Fikon',
+    volume: 'styck',
+    price: 5,
+    image: 'https://www.hebe.se/wp-content/uploads/2014/12/fikon-farska-br.jpg',
+  },
+  {
+    product: 'Papaya',
+    volume: '1 kg',
+    price: 43,
+    image: 'https://www.hebe.se/wp-content/uploads/2014/12/papaya-atmogen2.jpg',
+  },
+  {
+    product: 'Passionsfrukt',
+    volume: 'styck',
+    price: 12,
+    image: 'https://www.hebe.se/wp-content/uploads/2014/12/passionsfrukt21.jpg',
+  },
+  {
+    product: 'Physalis',
+    volume: '250g',
+    price: 32,
+    image: 'https://www.hebe.se/wp-content/uploads/2014/12/physalis.jpg',
+  },
+  {
+    product: 'Sharonfrukt',
+    volume: '500g',
+    price: 21,
+    image: 'https://www.hebe.se/wp-content/uploads/2014/12/sharon.jpg',
+  },
+]
+let meats = [
+  {
+    product: 'Kalv Högrev ',
+    volume: '1 kg',
+    price: 220,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/kalv/02.kalvhogrev.jpg?preset=scan-5:3',
+  },
+  {
+    product: 'Kalv Filé',
+    volume: '1 kg',
+    price: 318,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/kalv/05.kalvfile.jpg?preset=scan-5:3',
+  },
+  {
+    product: 'Oxfilé',
+    volume: '1 kg',
+    price: 322,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/not/05.oxfile.jpg?preset=scan-5:3',
+  },
+  {
+    product: 'Oxsvans',
+    volume: '1 kg',
+    price: 124,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/not/08.oxsvans.jpg?preset=scan-5:3',
+  },
+  {
+    product: 'Lammstek',
+    volume: '1 kg',
+    price: 234,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/lamm/04.lammstek.jpg?preset=scan-5:3',
+  },
+  {
+    product: 'Entrecôte',
+    volume: '1 kg',
+    price: 261,
+    image:
+      'https://www.scan.se/globalassets/scan/meat-guide/styckningsdetaljer/not/03.entrecote.jpg?preset=scan-5:3',
+  },
+]
+let pageInfo = [
+  {
+    category: 'Dairy',
+    description: 'Quality dairy products.',
+  },
+  {
+    category: 'Fruits',
+    description: 'Quality fruits products.',
+  },
+  {
+    category: 'Meats',
+    description: 'Quality meats products.',
   },
 ]
 
-function displayIngredients(list) {
+$('body').on('click', '#dairy-btn', () => {
+  displayProducts(dairy)
+  pageInfoElement.empty()
+  pageInfoElement.append(
+    `
+    <h1>
+    ${pageInfo[0].category}
+    </h1>
+    <p>
+    ${pageInfo[0].description}
+    </p>
+    `
+  )
+})
+$('body').on('click', '#fruits-btn', () => {
+  displayProducts(fruits)
+  pageInfoElement.empty()
+  pageInfoElement.append(
+    `
+    <h1>
+    ${pageInfo[1].category}
+    </h1>
+    <p>
+    ${pageInfo[1].description}
+    </p>
+    `
+  )
+})
+$('body').on('click', '#meats-btn', () => {
+  displayProducts(meats)
+  pageInfoElement.empty()
+  pageInfoElement.append(
+    `
+    <h1>
+    ${pageInfo[2].category}
+    </h1>
+    <p>
+    ${pageInfo[2].description}
+    </p>
+    `
+  )
+})
+
+function displayProducts(list) {
+  listOfIngredientsElement.empty()
+
   list.map(({ product, volume, price, image }) => {
     listOfIngredientsElement.append(
       `
@@ -54,4 +195,3 @@ function displayIngredients(list) {
     )
   })
 }
-displayIngredients(ingredients)
