b3424a28953dd2aa4752923be1d2c6777ac0dfdc
eyadal <31968847+eyadal@users.noreply.github.com>
2023-04-07 18:35:27 +0200

    Latest Changes

--- taskOne/main.js
+++ taskOne/main.js
@@ -1,71 +1,150 @@
-let listOfCars = $('#list-of-cars')
-let listOfNameElement = $('#list-of-names')
-// let carName = 'BMW'
-
-listOfCars.empty()
-
-// listOfCars.append(
-//   `
-//     <div class="car-card">
-//         <h2>${carName}</h2>
-//     </div>
-// `
-// )
-
-// const carBrands = ['Mercedes', 'BMW', 'Volvo', 'Tesla']
-// const carColors = ['Blue', 'Red', 'Yellow', 'Black']
-let names = ['Fadil', 'Eyad']
-let carsInfo = [
+const listOfVehicles = $('#list-of-vehicles')
+const searchInputElement = $('#search-input')
+// const searchInputElement = document.getElementById('search-input')
+let vehiclesInfo = [
   {
-    brand: 'Mercedes',
-    color: 'Blue',
+    brand: 'mercedes',
+    color: 'white',
+    year: 2023,
+    transmission: 'automatic',
+    price: '487000',
     image:
-      'https://images.pexels.com/photos/810357/pexels-photo-810357.jpeg?cs=srgb&dl=pexels-mike-b-810357.jpg&fm=jpg',
+      'https://www.mercedes-benz.se/passengercars/the-brand/amg-brand/mercedes-amg-brand/brand/amg-performance-vehicles/_jcr_content/swipeableteaserbox/par/swipeableteaser_1342/asset.MQ6.12.20210430120429.jpeg',
   },
   {
-    brand: 'BMW',
-    color: 'Black',
+    brand: 'bmw',
+    color: 'white',
+    year: 2022,
+    transmission: 'automatic',
+    price: '347 000',
     image:
-      'https://hips.hearstapps.com/hmg-prod/images/2022-bmw-x4-xdrive30i-606-edit-1654689776.jpg?crop=0.686xw:0.513xh;0.0272xw,0.448xh&resize=640:*',
+      'https://images.dealer.com/ddc/vehicles/2023/BMW/530e/Sedan/trim_xDrive_482c68/color/Alpine%20White-300-233%2C238%2C232-640-en_US.jpg',
   },
   {
-    brand: 'Volvo',
-    color: 'Yellow',
+    brand: 'volvo',
+    color: 'silver',
+    year: 2020,
+    transmission: 'manual',
+    price: '289 000',
     image:
-      'https://i.pinimg.com/originals/c1/91/07/c191071f571a8ef9e67b62ce11ee0b04.jpg',
+      'https://images.dealer.com/ddc/vehicles/2023/Volvo/S60/Sedan/trim_B5_Core_Dark_Theme_666a77/color/Silver%20Dawn%20Metallic-735-148%2C149%2C156-640-en_US.jpg?impolicy=downsize_bkpt&imdensity=1&w=520',
+  },
+  {
+    brand: 'tesla',
+    color: 'red',
+    year: 2021,
+    transmission: 'automatic',
+    price: '529 000',
+    image: 'https://www.pngmart.com/files/22/Tesla-Model-3-PNG-Image.png',
+  },
+  {
+    brand: 'mercedes',
+    color: 'silver',
+    year: 2022,
+    transmission: 'automatic',
+    price: '479 000',
+    image:
+      'https://bild.landrinsbil.se/cb:HqbC~437e0/w:512/h:341/q:80/https://landrinsbil.se/app/uploads/2021/07/Mercedes-AMG-A-klass-Halvkombi.jpg',
+  },
+  {
+    brand: 'bmw',
+    color: 'black',
+    year: 2022,
+    transmission: 'automatic',
+    price: '347 000',
+    image:
+      'https://www.bmw.com.pe/content/dam/bmw/common/all-models/5-series/sedan/2020/models-and-equipment/bmw-5-series-sedan-models-and-equipment-lines-01-01.jpg.asset.1587045994071.jpg',
+  },
+  {
+    brand: 'volvo',
+    color: 'white',
+    year: 2021,
+    transmission: 'manual',
+    price: '280 000',
+    image:
+      'https://vexstockimages.fastly.carvana.io/stockimages/2022_VOLVO_S60_B5%20MOMENTUM%20SEDAN%204D_WHITE_stock_desktop_1920x1080.png?v=1676381161.235',
   },
   {
     brand: 'Tesla',
-    color: 'Red',
+    color: 'blue',
+    year: 2023,
+    transmission: 'automatic',
+    price: '639 000',
     image:
-      'https://cdn.jdpower.com/JDP_2022%20Tesla%20Model%203%20Red%20Front%20Quarter%20View.jpg',
+      'https://mysterio.yahoo.com/mysterio/api/560D9CFA9F40C33DFAB0B40F9E9BC872D97704B1E154B1BE2E093622A4C723B0/autoblog/resizefill_w476_h266;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USD10TSC024D021001.jpg',
+  },
+  {
+    brand: 'mercedes',
+    color: 'black',
+    year: 2021,
+    transmission: 'automatic',
+    price: '379 000',
+    image:
+      'https://bild.landrinsbil.se/cb:HqbC~437e0/w:auto/h:auto/q:80/https://landrinsbil.se/app/uploads/2021/06/Mercedes-Benz-C-klass-Sedan.jpg',
+  },
+  {
+    brand: 'bmw',
+    color: 'blue',
+    year: 2022,
+    transmission: 'manual',
+    price: '447 000',
+    image:
+      'https://www.bmw.com.pe/content/dam/bmw/common/all-models/5-series/sedan/2020/models-and-equipment/bmw-5-series-sedan-models-and-equipment-lines-03-01.jpg.asset.1631533571837.jpg',
+  },
+  {
+    brand: 'volvo',
+    color: 'red',
+    year: 2019,
+    transmission: 'automatic',
+    price: '279 000',
+    image: 'https://imgd.aeplcdn.com/370x208/n/rzafkva_1604911.jpg?q=75',
+  },
+  {
+    brand: 'tesla',
+    color: 'grey',
+    year: 2022,
+    transmission: 'automatic',
+    price: '479 000',
+    image:
+      'https://static-assets.tesla.com/configurator/compositor?&bkba_opt=2&view=STUD_3QTR&size=1400&model=ms&options=$BP02,$ADPX2,$AU01,$APF1,$APH3,$APPB,$X028,$BTX5,$BS00,$BCMB,$CH04,$CF00,$CW02,$COSE,$X039,$IDCF,$X027,$DRLH,$DU00,$AF02,$FMP6,$FG02,$DCF0,$FR04,$X007,$X011,$INLFC,$PI01,$IX00,$X001,$LP01,$MI03,$X037,$MDLS,$DV4W,$X025,$X003,$PMNG,$PS01,$PK00,$X031,$PX00,$PF00,$X043,$TM00,$BR04,$REEU,$RFP2,$X014,$ME02,$QTFC,$SR07,$SP01,$X021,$SC04,$SU01,$TR00,$TIM3,$DSHG,$MT75A,$UTZW,$WTAS,$YFCC,$CPF0&crop=1400,850,300,130&',
   },
 ]
 
-function displayNames(listOfNames) {
-  listOfNames.map((name) => {
-    listOfNameElement.append(`
-        <h2>${name}</h2>
-        `)
+function displayCard(list) {
+  list.map(({ brand, color, year, transmission, price, image }) => {
+    let brandToUpperCase = brand.toUpperCase()
+    listOfVehicles.append(
+      `
+      <div class="car-card">
+        <h4>${brandToUpperCase}</h4>
+        <img src="${image}"width="300" height="300"/>
+        <hr />
+        <p>Year: ${year}</p>
+        <p>${transmission}</p>
+        <p>${color}</p>
+        <p>Price: ${price} sek</p>
+      </div>
+    `
+    )
   })
 }
-displayNames(names)
+// 1.0 create addeventlistener to input
 
-function displayCard(list) {
-  list
-    .map(({ brand, color, image }) => {
-      let brandToUpperCase = brand.toUpperCase()
-      listOfCars.append(
-        `
-    <div class="car-card">
-       <h2>${brandToUpperCase}</h2>
-       <h2>${color}</h2>
-       <img src="${image}"width="300" height="300"/>
-   </div>
-    `
-      )
-    })
+function filterCards() {
+  vehiclesInfo.filter(({ brand, color, year, transmission, price, image }) => {
+    //   let vehicleBrand = brand
+    //   let vehicleYear = year
+    //   let vehicleColor = color
+    //   let vehicleTransmission = transmission
+    //   let vehiclePrice = price
+    //   let vehicleImage = image
+    //   console.log(event.target.id)
+  })
 }
 
 // displayCarBrands(carBrands)
-displayCard(carsInfo)
+displayCard(vehiclesInfo)
+filterCards()
+
+// Eventhandlers
+searchInputElement.addEventListener('click', filterCards())
